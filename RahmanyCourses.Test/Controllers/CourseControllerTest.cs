51b20aeef897e246d0104406e6ac297a8527a1fb
Abdelrhman Ahmed <47417146+ZoRoOt3@users.noreply.github.com>
2023-12-20 12:35:13 +0200

    Starting UnitTesting by adding testing project, and start testing CourseController

---
+++ RahmanyCourses.Test/Controllers/CourseControllerTest.cs
@@ -0,0 +1,40 @@
+﻿using Application.Models;
+using Application.Queries.CourseQueries;
+using Application.Queries.UserQueries;
+using AutoMapper;
+using FakeItEasy;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using RahmanyCourses.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RahmanyCourses.Test.Controllers
+{
+    public class CourseControllerTest
+    {
+        [Fact]
+        public async Task GetTopRatedCourse_ShouldRetrunOkResult()
+        {
+            //Arrange
+            var mediatorFake = A.Fake<IMediator>();
+            var mapper = A.Fake<IMapper>();
+            var fakecourses = A.CollectionOfDummy<CourseReturnModel>(5).AsEnumerable();
+            A.CallTo(() => mediatorFake.Send(A<GetTopRatedCoursesQuery>._, A<CancellationToken>._))
+                .Returns(Task.FromResult(fakecourses));
+            var controller = new CourseController(mediatorFake, mapper);
+            
+            //Act
+            var result = await controller.GetTopRatedCourses();
+
+            //Assert
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            var model = Assert.IsAssignableFrom<IEnumerable<CourseReturnModel>>(okResult.Value);
+        }
+    }
+}
---
+++ RahmanyCourses/Controllers/CourseController.cs
@@ -0,0 +1,105 @@
+﻿using Application.Commands.CourseCommands;
+using Application.Commands.UserCommands;
+using Application.Queries.CourseQueries;
+using Application.Queries.UserQueries;
+using AutoMapper;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using RahmanyCourses.DTO;
+using RahmanyCourses.Models;
+using System.Security.Claims;
+
+namespace RahmanyCourses.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CourseController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        private readonly IMapper _mapper;
+
+        public CourseController(IMediator mediator, IMapper mapper)
+        {
+            _mediator = mediator;
+            _mapper = mapper;
+        }
+
+        // Get All Courses With Its Rates.
+        [HttpGet("get-all-courses")]
+        [Authorize]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> GetAllCourses()
+        {
+            var request = new GetCoursesQuery();
+            var result = await _mediator.Send(request);
+            return Ok(result);
+        }
+
+
+        [HttpGet("get-top-rated-courses")]
+        [ProducesResponseType(StatusCodes.Status200OK)]        
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> GetTopRatedCourses()
+        {
+            var request = new GetTopRatedCoursesQuery();
+            var result = await _mediator.Send(request);
+            return Ok(result);
+        }
+
+        [HttpGet("get-my-created-courses")]
+        [Authorize(Roles = "Instructor")]
+        public async Task<IActionResult> GetMyCourses()
+        {
+            int instructorId = int.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier));
+            var request = new GetCreatedCoursesCommand { UserId = instructorId };
+            var result = await _mediator.Send(request);
+            return Ok(result);
+        }
+
+        [HttpGet("get-course-by-id")]
+        public async Task<IActionResult> GetCourseById([FromQuery]GetCourseByIdQuery request)
+        {
+            var result = await _mediator.Send(request);
+            if (result == null)
+                return NotFound();
+            return Ok(result);
+        }
+
+        [HttpPost("add-course"), Authorize(Roles = "Instructor")]
+        public async Task<IActionResult> AddCourse(CourseModel model)
+        {
+            var request = _mapper.Map<AddCourseCommand>(model);
+            request.InstructorID = int.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier)!);
+            var result = await _mediator.Send(request);
+            return Ok(result);
+        }
+        [HttpPost("enroll-in-course"), Authorize]
+        public async Task<IActionResult> EnrollInCourse(int courseID)
+        {
+            int studentid = int.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier));
+            var request = new EnrollInCourseCommand { CourseID = courseID, StudentID = studentid };
+            var result = await _mediator.Send(request);
+            if(!result.IsCourseAvailable)
+                return NotFound(result);
+            if(!result.IsUserAllowedToEnroll) 
+                return BadRequest(new { Message = "You can't enroll in your courses" });
+            return Ok(new { CourseId = result.CourseID, studentid = request.StudentID });
+        }
+
+        [HttpPost("rate-course")]
+        [Authorize]
+        public async Task<IActionResult> RateCourse(RateCourseDTO dTO)
+        {
+            int studentid = int.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier));
+            var request = _mapper.Map<RateCourseCommand>(dTO);
+            request.StudentID = studentid;
+            var result = await _mediator.Send(request);
+            if (result == null)
+                return BadRequest("you aren't enrolled in this course");
+            return Ok(result);
+        }
+    }
+}
